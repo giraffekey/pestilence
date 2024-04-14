10683f597727b5962608cf622e5c5c84699015ab
giraffekey <giraffekey@tutanota.com>
2024-04-14 05:07:09 -0700

    Remove second level

--- src/levels.rs
+++ src/levels.rs
@@ -39,33 +39,5 @@ pub fn levels() -> Vec<Level> {
             ],
             initial_dna: 10,
         },
-        Level {
-            id: 1,
-            tilemap: vec![
-                vec![0, 1, 1, 1, 1, 1, 0, 0],
-                vec![1, 1, 1, 1, 1, 2, 1, 1],
-                vec![1, 2, 1, 1, 1, 1, 1, 1],
-                vec![1, 1, 1, 1, 1, 1, 1, 1],
-                vec![1, 1, 1, 2, 1, 1, 1, 0],
-                vec![0, 1, 1, 1, 1, 1, 0, 0],
-            ],
-            units: vec![
-                (UnitType::Scout, Position(0, 1)),
-                (UnitType::Scout, Position(2, 2)),
-                (UnitType::Sniper, Position(4, 3)),
-                (UnitType::Ballistic, Position(6, 0)),
-                (UnitType::Ballistic, Position(3, 5)),
-                (UnitType::Heavy, Position(5, 3)),
-                (UnitType::Commander, Position(7, 4)),
-            ],
-            obstacles: vec![
-                (Obstacle::Wall, Position(1, 1)),
-                (Obstacle::Wall, Position(3, 3)),
-                (Obstacle::Wall, Position(4, 2)),
-                (Obstacle::Wall, Position(6, 4)),
-                (Obstacle::Boulder, Position(4, 1)),
-            ],
-            initial_dna: 10,
-        },
     ]
 }
